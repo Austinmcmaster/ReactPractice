3ed3860238cad5c9f50d5c4e2cc0255072f1bbf3
Austin McMaster <Austinmcmaster4@yahoo.com>
2022-06-07 10:07:43 -1000

    Learn Props and inbedded styling

--- react-task-tracker/src/components/Header.js
+++ react-task-tracker/src/components/Header.js
@@ -1,10 +1,23 @@
+import PropTypes from 'prop-types'
 
-export const Header = () => {
+export const Header = ({title}) => {
   return (
       <header>
-          <h1>Task Tracker</h1>
+          <h1 style = {headingStyle}>{title}</h1>
       </header>
   )
+}
+
+Header.defaultProps = {
+    title: 'Task Tracker'
+}
+
+Header.propTypes = {
+    title: PropTypes.string.isRequired,
+}
+const headingStyle = {
+    color: 'red', 
+    backgroundColor:'black'
 }
 
 export default Header
