cbee2bca69bd32e59c3c5fc72a291efc19642b90
Austin McMaster <Austinmcmaster4@yahoo.com>
2022-06-08 22:41:03 -1000

    Learning Props and events with componenets

--- react-task-tracker/src/components/Header.js
+++ react-task-tracker/src/components/Header.js
@@ -2,12 +2,17 @@ import PropTypes from 'prop-types'
 import Button from './Button'
 
 export const Header = ({title}) => {
+
+    const onClick = () => {
+        console.log("Temp")
+    }
+
+
   return (
       <header className = 'header'>
           <h1>{title}</h1>
-          <Button color = 'Green' text = 'Hello' />
-          <Button color = 'Red' text = 'Hello1' />
-          <Button color = 'Yellow' text = 'Hello2' />
+          <Button color = 'Purple' text = 'Add' onClick = {onClick} />
+
       </header>
   )
 }
