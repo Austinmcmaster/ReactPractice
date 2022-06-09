5cb055e160f6812538a94080affbe1ee4c134cca
Austin McMaster <Austinmcmaster4@yahoo.com>
2022-06-08 22:56:46 -1000

    Learning States and useHooks

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -1,8 +1,22 @@
 import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+import {useState} from 'react'
+
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+        id: 2,
+        text: "Test",
+        day: "Feb 1",
+        reminder:true
+    }
+])
+
+
   return (
     <div className="container">
       <Header />
+      <Tasks tasks = {tasks} />
     </div>
   );
 }
