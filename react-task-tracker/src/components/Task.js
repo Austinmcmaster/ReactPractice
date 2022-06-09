03430c27a463580847bc94477fccd166dc07a458
Austin McMaster <Austinmcmaster4@yahoo.com>
2022-06-08 23:03:16 -1000

    Task components

---
+++ react-task-tracker/src/components/Task.js
@@ -0,0 +1,13 @@
+
+const task = ({task}) => {
+  return (
+    <div className = 'task'>
+        <h3>{task.text}</h3>
+        <p>
+            {task.day}
+        </p>
+    </div>
+  )
+}
+
+export default task
