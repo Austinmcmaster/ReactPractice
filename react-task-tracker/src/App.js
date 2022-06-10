3c0e43ed5567740745da447a28a8e0f999618879
Austin McMaster <Austinmcmaster4@yahoo.com>
2022-06-09 18:00:19 -1000

    Add Toggling of reminder and updating server data

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -3,6 +3,7 @@ import Tasks from "./components/Tasks";
 import {useState, useEffect} from 'react'
 import AddTask from "./components/AddTask";
 
+
 function App() {
   const [showAddTask, setShowAddTask] = useState(false)
   const [tasks, setTasks] = useState([])
@@ -23,11 +24,29 @@ function App() {
     return data
   }
 
+  const fetchTask = async (id) => {
+    const res = await fetch(`http://localhost:5000/tasks/${id}`)
+    const data = await res.json()
+
+    return data
+  }
+
 // Add Task
-const addTask = (task) => {
-  const id = Math.floor(Math.random() * 10000 ) + 1
-  const newTask = {id, ...task}
-  setTasks([...tasks, newTask])
+const addTask = async (task) => {
+  const res = await fetch('http://localhost:5000/tasks', {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json'
+    },
+    body: JSON.stringify(task)
+  })
+
+  const data = await res.json()
+  setTasks([...tasks,data])
+
+  // const id = Math.floor(Math.random() * 10000 ) + 1
+  // const newTask = {id, ...task}
+  // setTasks([...tasks, newTask])
 }
 
 
@@ -40,9 +59,22 @@ const deleteTask = async (id) => {
 }
 
 // Toggle Remindmer
-const toggleReminder = (id) => {
+const toggleReminder = async (id) => {
+  const taskToToggle = await fetchTask(id)
+  const updTask = {...taskToToggle,reminder : !taskToToggle.reminder}
+
+  const res = await fetch(`http://localhost:5000/tasks/${id}`,{
+    method: 'PUT',
+    headers: {
+      'Content-type': 'application/json'
+    },
+    body: JSON.stringify(updTask)
+  })
+
+  const data = await res.json()
+
   setTasks(tasks.map((task) => task.id === id ? 
-  {...task, reminder: !task.reminder}:task  ))
+  {...task, reminder: data.reminder}:task  ))
 }
 
 
