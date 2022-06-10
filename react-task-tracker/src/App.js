93d78732f27f41d8b5e7cebd1cc51008445b2ecc
Austin McMaster <Austinmcmaster4@yahoo.com>
2022-06-09 17:37:15 -1000

    Mock Backend for tutorial

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -1,18 +1,27 @@
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 import AddTask from "./components/AddTask";
 
 function App() {
   const [showAddTask, setShowAddTask] = useState(false)
-  const [tasks, setTasks] = useState([
-    {
-        id: 2,
-        text: "Test",
-        day: "Feb 1",
-        reminder:true
+  const [tasks, setTasks] = useState([])
+
+  useEffect(() => {
+    const getTasks = async () => {
+      const tasksFromServer = await fetchTasks()
+      setTasks(tasksFromServer)
     }
-])
+    getTasks()
+  }, [])
+
+  // Fetch Tasks
+  const fetchTasks = async () => {
+    const res = await fetch('http://localhost:5000/tasks')
+    const data = await res.json()
+
+    return data
+  }
 
 // Add Task
 const addTask = (task) => {
