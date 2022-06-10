ec45278adfb41d5e9374f7a64b5cbe3fb95d7e62
Austin McMaster <Austinmcmaster4@yahoo.com>
2022-06-09 16:39:34 -1000

    Get the ability to add task

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -13,7 +13,15 @@ function App() {
     }
 ])
 
-// Delet Task
+// Add Task
+const addTask = (task) => {
+  const id = Math.floor(Math.random() * 10000 ) + 1
+  const newTask = {id, ...task}
+  setTasks([...tasks, newTask])
+}
+
+
+// Delete Task
 const deleteTask = (id) => {
   setTasks(tasks.filter((task) => task.id !== id))
 }
@@ -28,7 +36,7 @@ const toggleReminder = (id) => {
   return (
     <div className="container">
       <Header />
-      <AddTask/>
+      <AddTask onAdd ={addTask}/>
       {tasks.length > 0 ? 
       (<Tasks tasks = {tasks} onDelete = {deleteTask}
       onToggle = {toggleReminder} />) 
