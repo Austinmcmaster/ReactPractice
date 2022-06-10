540ead2dd368d2bae989188d9a4deb4ddbb2170b
Austin McMaster <Austinmcmaster4@yahoo.com>
2022-06-09 18:27:58 -1000

    Finish Tutorial Project

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -5,6 +5,7 @@ import AddTask from "./components/AddTask";
 import Footer from "./components/Footer";
 
 
+
 function App() {
   const [showAddTask, setShowAddTask] = useState(false)
   const [tasks, setTasks] = useState([])
